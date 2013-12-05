5cf546d5ac10f5c528fef9240238a80bde60f625
Ryan Cromwell <ryan@echelontouch.com>
2013-12-05 00:39:12 -0500

    zsh choice on startup

---
+++ .tmux.start.sh
@@ -0,0 +1,26 @@
+#!/bin/sh 
+export PATH=$PATH:~/.local/bin:/usr/local/bin
+[ "$TMUX" == "" ] || exit 0 
+tmux has-session -t _default || tmux new-session -s _default -d
+PS3="Please choose your session: "
+options=($(tmux list-sessions -F "#S") "NEW SESSION" "BASH")
+echo "Available sessions"
+echo "------------------"
+echo " "
+select opt in "${options[@]}"
+do
+    case $opt in
+        "NEW SESSION")
+            read -p "Enter new session name: " SESSION_NAME
+            tmux new -s "$SESSION_NAME"
+            break
+            ;;
+        "BASH")
+            bash --login
+            break;;
+        *) 
+            tmux attach-session -t $opt 
+            break
+            ;; 
+    esac
+done
